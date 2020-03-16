44162d0c26d6f00e7cb577662bf0114254842ae6
Julio Silva <julio@adsim.com.br>
2020-03-16 16:50:18 -0300

    Implementação de auth2 do google

--- src/main/java/com/springboot/api/controllers/IndexController.java
+++ src/main/java/com/springboot/api/controllers/IndexController.java
@@ -1,9 +1,26 @@
 package com.springboot.api.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * IndexController
@@ -11,13 +28,62 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController implements ErrorController {
 
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    private static String authorizationRequestBaseUri = "oauth2/authorization";
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @RequestMapping(value = "/")
-    public String index() {
+    public String index(Model model, OAuth2AuthenticationToken authentication) {
+
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+
+        
+        String userInfoEndpointUri = new String("");
+        if(client != null) {
+            userInfoEndpointUri = client.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUri();
+        }
+        
+        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+                .getTokenValue());
+
+            HttpEntity<?> entity = new HttpEntity<>("", headers);
+            ResponseEntity<Map>response = restTemplate
+                .exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+
+            Map userAttributes = response.getBody();
+            model.addAttribute("name", userAttributes.get("name"));
+        }
+
         return "index";
     }
     
     @RequestMapping(value = "/login")
-    public String login() {
+    public String login(Model model) {
+
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository).as(Iterable.class);
+        if (type != ResolvableType.NONE && 
+
+        ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+    
+        clientRegistrations.forEach(registration -> 
+        oauth2AuthenticationUrls.put(registration.getClientName(), 
+        authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+
         return "login";
     }
     
@@ -27,11 +93,18 @@ public class IndexController implements ErrorController {
     public String error() {
         return "https://github.com/juliosilvacwb/cadastro-pessoa";
     }
-
+    
+    
     @Override
     @RequestMapping("/error")
     public String getErrorPath() {
         return "index";
+    }
+    
+    @RequestMapping(value = "/login/oauth2/code/google")
+    @ResponseBody
+    public String alth2() {
+        return "home";
     }
     
 }
--- src/main/java/com/springboot/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/springboot/api/security/config/WebSecurityConfig.java
@@ -1,15 +1,28 @@
 package com.springboot.api.security.config;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -20,8 +33,14 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@PropertySource("classpath:application.properties")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Resource
+    private Environment env;
+
+    private static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
+    
     private static final String[] AUTH_WHITELIST = {
         "/source",
         "/actuator/**",
@@ -53,8 +72,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
             .defaultSuccessUrl("/home")
             .loginPage("/login").permitAll()
+            .and()
+            .httpBasic()
         .and()
-            .httpBasic();
+            .oauth2Login()
+            .defaultSuccessUrl("/home")
+            .loginPage("/login").permitAll()
+            .clientRegistrationRepository(clientRegistrationRepository())
+            .authorizedClientService(authorizedClientService());
 
         httpSecurity.headers().cacheControl();
         
@@ -80,6 +105,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+ 
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+    
+        return new InMemoryOAuth2AuthorizedClientService(
+        clientRegistrationRepository());
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<ClientRegistration> registrations = Arrays.asList(getRegistration("google"));
+
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+    private ClientRegistration getRegistration(String client) {
+        System.out.println(CLIENT_PROPERTY_KEY + client + ".client-id");
+        String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
+
+        if (clientId == null) {
+            return null;
+        }
+
+        String clientSecret = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-secret");
+
+        if (client.equals("google")) {
+            return CommonOAuth2Provider.GOOGLE.getBuilder(client).clientId(clientId).clientSecret(clientSecret).build();
+        }
+
+        return null;
     }
 
 }
